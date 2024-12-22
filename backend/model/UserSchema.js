488e1a02f4819a7ad1d3f9f4f4225eb35723889d
kishu <souravpoddar6677@gmail.com>
2024-12-22 22:51:44 +0530

    project created

---
+++ backend/model/UserSchema.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: { type: String },
+  email: { type: String, required: true },
+  passwd: { type: String, required: true },
+});
+const UserModel = mongoose.model("UserModel", userSchema);
+
+const todoSchema = mongoose.Schema({
+  userId: { type: mongoose.Types.ObjectId, ref: UserModel },
+  todo: { type: String },
+});
+const TodoModel = mongoose.model("TodoModel", todoSchema);
+
+module.exports = { UserModel, TodoModel };
---
+++ backend/server.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+// routes
+const UserRoutes = require("./route/UserRoutes");
+
+const app = express();
+app.use(express.json());
+
+async function main() {
+  await mongoose
+    .connect(process.env.DB_URL)
+    .then(console.log("Database connected securely"))
+    .catch((err) => console.log(err));
+
+  app.listen(process.env.PORT, () => {
+    console.log("Server listening");
+  });
+}
+
+app.get("/", async (req, res) => {
+  res.send("Heiii");
+});
+
+app.use("/user", UserRoutes);
+main();
