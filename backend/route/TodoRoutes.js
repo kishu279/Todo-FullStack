4009adb8edcbf2cdd9290021fdb604a323d4d775
kishu <souravpoddar6677@gmail.com>
2024-12-23 01:01:39 +0530

    create and add todos are added

--- backend/route/TodoRoutes.js
+++ backend/route/TodoRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 
-const { UserModel, TodoModel } = require("./../model/UserSchema");
+const { TodoModel } = require("./../model/UserSchema");
 const auth = require("../middleware/auth");
 
 const router = express.Router();
 
 // create todos
-router.post("/create", auth, (req, res) => {
+router.post("/create", auth, async (req, res) => {
   const { title } = req.body;
   const id = req.userId; // fetched id from auth middleware will be used in todo model
 
@@ -17,15 +17,34 @@ router.post("/create", auth, (req, res) => {
     });
   }
 
-  TodoModel.create({
+  // todo created if title found valid
+  await TodoModel.create({
     userId: id,
     todo: title,
   });
 
   return res.status(200).json({
-    success: false,
+    success: true,
     message: "successfully created",
   });
+});
+
+router.get("/view", auth, async (req, res) => {
+  const id = req.userId;
+
+  // find the todo with the help of user id
+  const data = await TodoModel.find({ userId: id });
+
+  if (data.length == 0) {
+    res.status(300).json({
+      success: true,
+      message: "add todos then view todos",
+    });
+  }
+
+  return res.status(200).json({
+    data,
+  }); // data is in the form of array of objects
 });
 
 module.exports = router;
